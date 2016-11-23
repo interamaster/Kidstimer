5e2c5aa4da952b965428a554023c37ea6d0311d3
joseramondelgadoMACPRO <interamaster@gmail.com>
2016-11-23 18:47:08 +0100

    V091 AÑADIDO NOTIFS DESDE FIREBASE SIN USERS

---
+++ app/src/main/java/com/sfc/jrdv/kidstimer/firebase/MyFirebaseMessagingService.java
@@ -0,0 +1,21 @@
+package com.sfc.jrdv.kidstimer.firebase;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by joseramondelgado on 04/11/16.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.e("FIREBASE", remoteMessage.getNotification().getBody());
+    }
+
+
+
+}
